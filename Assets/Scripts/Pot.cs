4d718761540fdc1b0db192c371476020eb165a23
Harry-Hopkinson <harryjhopkinson@gmail.com>
2021-02-01 18:18:27 +0000

    Fixed the camera and added better pot animations and damage

--- Assets/Scripts/Pot.cs
+++ Assets/Scripts/Pot.cs
@@ -21,5 +21,12 @@ public class Pot : MonoBehaviour
     public void Smash()
     {
         anim.SetBool("smash", true);
+        StartCoroutine(breakCo());
+    }
+
+    IEnumerator breakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
     }
 }
