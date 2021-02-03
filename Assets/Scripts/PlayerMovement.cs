ee088d79d59d8aa03b5fc584cae1fcc0b7ebd514
Harry-Hopkinson <harryjhopkinson@gmail.com>
2021-02-03 11:28:09 +0000

    Added the enemy log animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
